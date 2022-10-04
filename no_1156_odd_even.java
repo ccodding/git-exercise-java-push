62660ecb3d222812ffb331563448a72a46b3d5b8
nameing <>
2022-10-04 15:11:03 +0900

    code1156

---
+++ no_1156_odd_even.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class no_1156_odd_even {
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+
+        if(num%2 == 1){
+            System.out.println("odd");
+        }
+        else{
+            System.out.println("even");
+        }
+    }
+}
+
+/*
+int num = 5;
+if(num == 5){
+ system.out.println("odd");
+ */
